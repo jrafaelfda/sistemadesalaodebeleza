e7693358b862ec104083194c14597b6025873e29
João Rafael <95690569+jrafaelfda@users.noreply.github.com>
2023-06-12 11:29:16 -0300

    Delete salao-beleza directory

--- salao-beleza/src/main/java/com/unicap/salaobeleza/repository/PacoteServicosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unicap.salaobeleza.repository;
-
-import com.unicap.salaobeleza.model.PacoteServicos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PacoteServicosRepository extends JpaRepository<PacoteServicos, Long> {
-}
